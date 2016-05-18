6bd90957d39f594d3f8c50b7e9321ec22c6352f7
Phisten <ans10528@gmail.com>
2016-05-18 11:01:50 +0800

    DetectServer OK / 正在處理master連接
    network onmain未處理
    需以新thread處理連接

--- AndroFPV/app/src/main/java/phisten/androfpv/MainActivity.java
+++ AndroFPV/app/src/main/java/phisten/androfpv/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -11,25 +13,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     static final String LogTag_Error = "Error";
     private static Toast toast;
+    SocketChannelPool scp;
 
-    private static void makeTextAndShow(final Context context, final String text, final int duration) {
-        if (toast == null) {
-            //如果還沒有用過makeText方法，才使用
-            toast = android.widget.Toast.makeText(context, text, duration);
-        } else {
-            toast.setText(text);
-            toast.setDuration(duration);
-        }
-        toast.show();
-    }
-
-    SocketChannelPool scp = SocketChannelPool.getInstance();
+    EditText UI_EditText_TargetIp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +31,19 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //scp init
+        scp = SocketChannelPool.getInstance();
         //取得IP 寫入資訊列
         String localIP = getMyIp();
         Log.i("Info", "curIP: " + getMyIp());
         scp.LocalIP = localIP;
         SetTextViewText(R.id.textView_LocalIP,R.string.textview_LocalIP,localIP);
 
+        scp.UiHandler = handler;
 
         //UI Init
+        UI_EditText_TargetIp = (EditText) findViewById(R.id.IpText);
+
         Button DectectBtn = (Button) findViewById(R.id.DectectBtn);
         DectectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,10 +75,18 @@ public class MainActivity extends AppCompatActivity {
         ConnectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //向指定IP請求masterCh連接
+                SocketChannelPool scp;
+                scp = SocketChannelPool.getInstance();
 
+                //向指定IP請求masterCh連接
+                //EditText ipEditText = (EditText) findViewById(R.id.IpText);
+                String hostIp = UI_EditText_TargetIp.getText().toString();
+
+                makeTextAndShow(getApplicationContext(),"TryConnect IP = " + hostIp,Toast.LENGTH_SHORT);
+                scp.MasterChannelConnect(hostIp);
             }
         });
+
 
 
 
@@ -97,6 +103,42 @@ public class MainActivity extends AppCompatActivity {
         //利用位移運算和AND運算計算IP
         String ip = String.format("%d.%d.%d.%d",(ipAddress & 0xff),(ipAddress >> 8 & 0xff),(ipAddress >> 16 & 0xff),(ipAddress >> 24 & 0xff));
         return ip;
+    }
+
+    //UI Control -------------------------------------------------------------------------------------------
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            String MsgString = (String)msg.obj;
+            if (!MsgString.equals(""))
+            {
+
+            }
+
+            switch (msg.what) {
+                case SocketChannelPool.HANDLERMSG_SHOWTEXT:
+                    Log.d(LogTag.OutputTest, "handleMessage: " + MsgString);
+                    makeTextAndShow(getApplicationContext(), MsgString ,Toast.LENGTH_SHORT);
+                    break;
+                case SocketChannelPool.HANDLERMSG_SET_TARGET_IP:
+                    Log.d(LogTag.OutputTest, "SetTargetIP: " + MsgString);
+                    UI_EditText_TargetIp.setText(MsgString);
+                    makeTextAndShow(getApplicationContext(), MsgString ,Toast.LENGTH_SHORT);
+                    break;
+
+            }
+        }
+    };
+
+    private static void makeTextAndShow(final Context context, final String text, final int duration) {
+        if (toast == null) {
+            //如果還沒有用過makeText方法，才使用
+            toast = android.widget.Toast.makeText(context, text, duration);
+        } else {
+            toast.setText(text);
+            toast.setDuration(duration);
+        }
+        toast.show();
     }
 
     //設定資訊列內的文字,titleId輸入0可僅顯示text
--- AndroFPV/app/src/main/java/phisten/androfpv/SocketChannelPool.java
+++ AndroFPV/app/src/main/java/phisten/androfpv/SocketChannelPool.java
@@ -1,17 +1,21 @@
 package phisten.androfpv;
 
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.Message;
 import android.util.Log;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -26,32 +30,48 @@ public class SocketChannelPool {
     }
     static private SocketChannelPool ourInstance = new SocketChannelPool();
 
-
     //Channel
-    public ServerSocketChannel MasterServerChannel;
-    public SocketChannel MasterChannel;
-    public DatagramChannel DetecterChannel;
-    public int DataChannelCount = 3;
-    public ArrayList<DatagramChannel> DataChannels;
+    private Selector MasterThreadSelector;
+    //private SelectionKey MasterThreadSelectionKey;
+    private ServerSocketChannel MasterServerChannel;
+    private SocketChannel MasterChannel;
+    private DatagramChannel DetecterChannel;
+    private int DataChannelCount = 3;
+    private ArrayList<DatagramChannel> DataChannels;
 
     //Connect Info
+    public boolean isServer = true;
     public String LocalIP;
     InetSocketAddress RemoteISA;
     private String RemoteIP;
-    protected Handler handler;
     protected Message msg;
     static final int MasterServerChannelPort = 27390;
     static final int DetecterChannelPort = 27380;
 
+
+    public Handler UiHandler;
+    static public final int HANDLERMSG_SHOWTEXT = 1;
+    static public final int HANDLERMSG_SET_TARGET_IP = 2;
+    static public final int HANDLERMSG_ACCEPT = 3;
+
     private SocketChannelPool() {
         DataChannels = new ArrayList<>();
         try {
-            MasterServerChannel = ServerSocketChannel.open();
-            MasterChannel = SocketChannel.open();
-            DetecterChannel = DatagramChannel.open();
-            MasterServerChannel.socket().bind(new InetSocketAddress(SocketChannelPool.MasterServerChannelPort));
-            DetecterChannel.socket().bind(new InetSocketAddress(SocketChannelPool.DetecterChannelPort));
+            MasterServerChannelInit();
 
+            DetecterChannel = DatagramChannel.open();
+            DetecterChannel.socket().bind(new InetSocketAddress(SocketChannelPool.DetecterChannelPort));
+            DetecterChannel.configureBlocking(false);
+
+            MasterThreadSelector = Selector.open();
+
+            SelectionKey k1 = MasterServerChannel.register(MasterThreadSelector,SelectionKey.OP_ACCEPT);
+            SelectionKey k3 = DetecterChannel.register(MasterThreadSelector,SelectionKey.OP_READ);
+
+//            Log.d(LogTag.Test, "SocketChannelPool: MasterChannelThread TryStart");
+            MasterChannelThread = new Thread(MasterChannelRunnable);
+            MasterChannelThread.start();
+//            Log.d(LogTag.Test, "SocketChannelPool: MasterChannelThread Start");
         } catch (IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
@@ -59,46 +79,111 @@ public class SocketChannelPool {
         }
     }
 
+
+
     // ThreadControl --------------------------------------------------------------------
-    Handler DetecterChannelHandler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-        }
-    };
-    Thread DetecterChannelThread;
-    private Runnable DetecterChannelRun = new Runnable() {
-        @Override
-        public void run() {
-            // TODO: getdata
-
-
-            handler.sendMessage(msg);
-        }
-    };
-
-    HandlerThread MasterChannelHandlerThread = new HandlerThread("Master");
+//    Handler MasterChannelHandler = new Handler(){
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//        }
+//    };
+    Thread MasterChannelThread;
     private Runnable MasterChannelRunnable = new Runnable() {
         @Override
         public void run() {
+            Log.d(LogTag.Test, "run: MasterChannelThread Start");
+            while(true) {
+                //send Queue --------------------------------------------------------
+
+                //recive Event ------------------------------------------------------
+                int readyChannels = 0;
+                try {
+                    readyChannels = MasterThreadSelector.select();
+
+                    //Log.d(LogTag.Test, "readyChannels=" + readyChannels);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if(readyChannels == 0) continue;
+                Set selectedKeys = MasterThreadSelector.selectedKeys();
+                Iterator keyIterator = selectedKeys.iterator();
+                while(keyIterator.hasNext()) {
+                    SelectionKey key = (SelectionKey) keyIterator.next();
+                    if (key.channel().equals(DetecterChannel))
+                    {
+                        if(key.isAcceptable()) {
+                            // a connection was accepted by a ServerSocketChannel.
+                        } else if (key.isConnectable()) {
+                            // a connection was established with a remote server.
+                        } else if (key.isReadable()) {
+                            //接收資料進byteBuf
+                            ByteBuffer byteBuf = ByteBuffer.allocate(5);
+                            byteBuf.clear();
+                            try {
+                                SocketAddress srcSocketAddr = DetecterChannel.receive(byteBuf);
+                                Log.d(LogTag.OutputTest, "run: srcSocketAddr = " + srcSocketAddr.toString());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            byteBuf.flip();
+
+                            //將資料由byteBuf讀出  第一位元為PacketType,用於分辨資料處理方式
+                            byte[] srcIp = new byte[4];
+                            byte PacketType = byteBuf.get();
+                            if (PacketType == Packet.PacketType.MasterChannelConnectRequest.toByte())
+                            {
+                                Log.d(LogTag.OutputTest, "PacketType == MasterChannelConnectRequest");
+                                byteBuf.get(srcIp);
+
+                                String srcIPstr = HostIpConvertToString(srcIp);
+                                UiHandler.sendMessage(UiHandler.obtainMessage(HANDLERMSG_SET_TARGET_IP,srcIPstr));
+                            }
+                        }
+                    }else if (key.channel().equals(MasterChannel))
+                    {
+                        if(key.isConnectable()) {
+                            // a connection was established with a remote server.
+                        } else if (key.isReadable()) {
+
+                        } else if (key.isWritable()) {
+                            // a channel is ready for writing
+                        }
+                    }else if (key.channel().equals(MasterServerChannel))
+                    {
+                        if(key.isAcceptable()) {
+                            try {
+                                if (MasterChannel != null){
+                                    MasterChannel.close();
+                                }
+                                MasterChannel = MasterServerChannel.accept();
+                                SelectionKey k2 = MasterChannel.register(MasterThreadSelector,SelectionKey.OP_READ | SelectionKey.OP_WRITE | SelectionKey.OP_CONNECT);
+                                UiHandler.sendMessage(UiHandler.obtainMessage(HANDLERMSG_ACCEPT,"accept"));
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else if (key.isConnectable()) {
+                            // a connection was established with a remote server.
+                        }
+                    }
 
 
 
-
-
+                    keyIterator.remove();
+                }
+            }
 
 
 
         }
     };
-
-
-    private Runnable ReceiveRunnable = new Runnable() {
-        @Override
-        public void run() {
-
-        }
-    };
+//    private Runnable ReceiveRunnable = new Runnable() {
+//        @Override
+//        public void run() {
+//
+//        }
+//    };
     private Runnable SendRunnable = new Runnable() {
         @Override
         public void run() {
@@ -107,9 +192,45 @@ public class SocketChannelPool {
     };
 
 
+    // public method --------------------------------------------------------------------
+    public int MasterServerChannelInit()
+    {
+        try {
+            MasterServerChannel = ServerSocketChannel.open();
+            MasterServerChannel.socket().bind(new InetSocketAddress(SocketChannelPool.MasterServerChannelPort));
+            MasterServerChannel.configureBlocking(false);
+            isServer = true;
 
-    // ReceiveData --------------------------------------------------------------------
-    // SendData --------------------------------------------------------------------
+            if (MasterChannel != null && MasterChannel.isOpen()) {
+                MasterChannel.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    public int MasterChannelConnect(String hostIp) {
+        try {
+            if (MasterChannel != null && MasterChannel.isOpen()) {
+                MasterChannel.close();
+            }
+            MasterChannel = SocketChannel.open();
+            MasterChannel.configureBlocking(false);
+            SelectionKey k2 = MasterChannel.register(MasterThreadSelector,SelectionKey.OP_READ | SelectionKey.OP_WRITE | SelectionKey.OP_CONNECT);
+            isServer = false;
+            if (MasterServerChannel != null && MasterServerChannel.isOpen()) {
+                MasterServerChannel.close();
+            }
+
+            Log.d(LogTag.OutputTest, "MasterChannelConnect: " + hostIp + ":" + MasterServerChannelPort);
+            MasterChannel.connect(new InetSocketAddress(hostIp,MasterServerChannelPort));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    // ReceiveData ----------------------------------------------------------------------
+    // SendData -------------------------------------------------------------------------
     public int DetectServer(String targetIP) throws Exception {
         Log.d(LogTag.OutputTest, "DetectServer: " +  targetIP + " , local:" + LocalIP);
         Thread t = new Thread(new Runnable() {
@@ -127,11 +248,13 @@ public class SocketChannelPool {
                     detectPacketBuf.put(Packet.PacketType.MasterChannelConnectRequest.toByte());
                     //填入本地IP
                     detectPacketBuf.put(SocketChannelPool.HostIpConvertToBytes(LocalIP));
+                    //Log.d(LogTag.Test, "run: " + HostIpConvertToString(HostIpConvertToBytes(LocalIP)));
                     detectPacketBuf.flip();
 
                     //發出封包
                     InetSocketAddress inetSocketAddr = new InetSocketAddress(targetIP,DetecterChannelPort);
                     int bytesSent = DetecterChannel.send(detectPacketBuf,inetSocketAddr);
+
                     Log.d(LogTag.OutputTest,"DetectServer: bytesSent = " + String.format("%d",bytesSent));
                 }catch(Exception e) {
                     Log.e(LogTag.Error,e.getMessage());
@@ -174,30 +297,31 @@ public class SocketChannelPool {
         }
     }
     /*將字串表示的IP轉換為4個int組成的陣列*/
-    static public int[] HostIpConvertToInts(String hostIp) throws Exception {
+    static public int[] HostIpConvertToInts(String hostIp){
         String[] ipStrArr = hostIp.split("\\.");
-        if (ipStrArr.length != 4) {
-            throw new Exception("func: HostIpConvertToInts IP Input Error(String[] ipStrArr.length == " + ipStrArr.length + " != 4 ) hostIp = " + hostIp.toString());
-        }
         int[] ints = new int[4];
+        if (ipStrArr.length != 4) {
+            Log.e(LogTag.Error, "HostIpConvertToInts:  IP Input Error(String[] ipStrArr.length != 4)", new Exception("func: HostIpConvertToInts IP Input Error(String[] ipStrArr.length == " + ipStrArr.length + " != 4 ) hostIp = " + hostIp.toString()));
+            return ints;
+        }
         for (int i = 0; i < 4; i++) {
             ints[i] = Integer.parseInt(ipStrArr[i]);
         }
         return ints;
     }
     /*將4個int組成之陣列所表示的IP轉換為字串表示*/
-    static public String HostIpConvertToString(int[] hostIp) throws Exception {
+    static public String HostIpConvertToString(int[] hostIp){
         if (hostIp.length != 4) {
-            throw new Exception("func: HostIpConvertToString IP Input Error(byte[] hostIp.length == " + hostIp.length + " != 4)");
+            Log.e(LogTag.Error, "HostIpConvertToInts:  IP Input Error(int[] hostIp.length != 4)", new Exception("func: HostIpConvertToString IP Input Error(byte[] hostIp.length == " + hostIp.length + " != 4)"));
         }
         String str1 = String.format("%d.%d.%d.%d",hostIp[0],hostIp[1],hostIp[2],hostIp[3]);
         Log.d(LogTag.OutputTest,str1);
         return str1;
     }
     /*將4個byte組成之陣列所表示的IP轉換為字串表示*/
-    static public String HostIpConvertToString(byte[] hostIp) throws Exception {
+    static public String HostIpConvertToString(byte[] hostIp){
         if (hostIp.length != 4) {
-            throw new Exception("func: HostIpConvertToString IP Input Error(byte[] hostIp.length == " + hostIp.length + " != 4)");
+            Log.e(LogTag.Error, "HostIpConvertToInts:  IP Input Error(byte[] hostIp.length != 4)", new Exception("func: HostIpConvertToString IP Input Error(byte[] hostIp.length == " + hostIp.length + " != 4)"));
         }
         int intConverter = 0;
         String str1 = String.format("%d.%d.%d.%d",intConverter | hostIp[0],intConverter | hostIp[1],intConverter | hostIp[2],intConverter | hostIp[3]);
@@ -205,10 +329,11 @@ public class SocketChannelPool {
         return str1;
     }
     /*將字串表示的IP轉換為4組byte組成的陣列*/
-    static public byte[] HostIpConvertToBytes(String hostIp) throws Exception {
+    static public byte[] HostIpConvertToBytes(String hostIp){
         String[] ipStrArr = hostIp.split("\\.");
         if (ipStrArr.length != 4) {
-            throw new Exception("func: HostIpConvertToInts IP Input Error(String[] ipStrArr.length == " + ipStrArr.length + " != 4 ) hostIp = " + hostIp.toString());
+            Log.e(LogTag.Error, "HostIpConvertToInts:  IP Input Error(String[] ipStrArr.length != 4)"
+            , new Exception("func: HostIpConvertToInts IP Input Error(String[] ipStrArr.length == " + ipStrArr.length + " != 4 ) hostIp = " + hostIp.toString()));
         }
         byte[] bs = new byte[4];
         for (int i = 0; i < 4; i++) {
@@ -217,9 +342,10 @@ public class SocketChannelPool {
         return bs;
     }
     /*將4個int組成之陣列所表示的IP轉換為byte陣列*/
-    static public byte[] HostIpConvertToBytes(int[] hostIp) throws Exception {
+    static public byte[] HostIpConvertToBytes(int[] hostIp){
         if (hostIp.length != 4) {
-            throw new Exception("func: HostIpConvertToString IP Input Error(byte[] hostIp.length == " + hostIp.length + " != 4)");
+            Log.e(LogTag.Error, "HostIpConvertToInts:  IP Input Error(int[] hostIp.length != 4)"
+                    , new Exception("func: HostIpConvertToString IP Input Error(int[] hostIp.length == " + hostIp.length + " != 4)"));
         }
         byte[] bs = new byte[4];
         for (int i = 0; i < 4; i++) {
@@ -231,5 +357,6 @@ public class SocketChannelPool {
     // return Code --------------------------------------------------------------------
     static public final int Succeed = 0;
     static public final int UndefinedError = -1;
+
 
 }
